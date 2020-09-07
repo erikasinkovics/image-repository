8968745b7d36f8a8c0b1b56ad5aa71d7115ce6e8
Erika Sinkovics <erika.sinkovics@outlook.com>
2020-09-06 23:41:19 -0400

    Created Image controller

--- app/controllers/image_controller.rb
+++ app/controllers/image_controller.rb
@@ -1,16 +1,34 @@
 class ImageController < ApplicationController
   def index
+    @images = Image.all
   end
 
   def show
+    @image = Image.find(params[:id])
   end
 
   def new
+    @image = Image.new
   end
 
   def create
+    @image = Image.new(image_params)
+    if @image.save
+      redirect_to @image
+    else
+      flash.now[:error] = "Could not save image"
+    end
   end
 
   def destroy
+    @image = Image.find(params[:id])
+    @image.destroy
+    redirect_to '/image/index', :notice => "Your image has been deleted"
+  end
+
+  private
+
+  def image_params
+    params.require(:image).permit(:description, :file_name)
   end
 end
