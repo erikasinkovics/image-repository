d083f52775e8da1b1cc91b5d34382399c751df06
Erika Sinkovics <erika.sinkovics@outlook.com>
2020-09-07 23:54:14 -0400

    Added params manually to test Cloudinary upload

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,10 +1,11 @@
 class ImagesController < ApplicationController
+  before_action :set_image, only: [:show, :destroy]
+
   def index
     @images = Image.all
   end
 
   def show
-    @image = Image.find(params[:id])
   end
 
   def new
@@ -12,23 +13,32 @@ class ImagesController < ApplicationController
   end
 
   def create
-    @image = Image.new(image_params)
+    @image = Image.new({
+      description: params[:image][:description],
+      file_name: params[:image][:photo].original_filename,
+      user_id: current_user.id,
+      photo: params[:image][:photo]
+    })
     if @image.save
-      redirect_to @image
+      redirect_to user_images_path(params[:user_id]), notice: "Image was successfully uploaded!"
     else
       flash.now[:error] = "Could not save image"
     end
   end
 
   def destroy
-    @image = Image.find(params[:id])
+    @image.photo.purge
     @image.destroy
-    redirect_to '/image/index', :notice => "Your image has been deleted"
+    redirect_to user_images_path(params[:user_id]), :notice => "Your image has been deleted."
   end
 
   private
 
+  def set_image
+    @image = Image.find(params[:id])
+  end
+
   def image_params
-    params.require(:image).permit(:description, :file_name, :photo)
+    params.require(:image).permit(:description, :file_name, :photo, :user_id)
   end
 end
